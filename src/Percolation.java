dd27fcc62387ff78959e2a43e612a6cbb10a3bab
nick serebrennikov <non.manifold@gmail.com>
2016-09-11 23:01:25 +0300

    backwash elimination by second sites collection without a bottom site

--- src/Percolation.java
+++ src/Percolation.java
@@ -4,11 +4,11 @@ public class Percolation {
 
     private static final int[][] DIRECTIONS = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     private final WeightedQuickUnionUF sites;
+    private final WeightedQuickUnionUF sitesTopOnly;
     private final int size;
     private final boolean[] opened;
     private final int topVirtualSite;
     private final int bottomVirtualSite;
-    private final int size1D;
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
@@ -16,8 +16,9 @@ public class Percolation {
             throw new java.lang.IllegalArgumentException("n must be at least 1");
         }
         size = n;
-        size1D = n * n;
+
         sites = new WeightedQuickUnionUF(n * n + 2);
+        sitesTopOnly = new WeightedQuickUnionUF(n * n + 1);
         opened = new boolean[n * n + 2];
         topVirtualSite = 0;
         bottomVirtualSite = xyTo1D(size, size) + 1;
@@ -33,6 +34,7 @@ public class Percolation {
         opened[xy] = true;
         if (row == 1) {
             sites.union(sites.find(topVirtualSite), sites.find(xy));
+            sitesTopOnly.union(sitesTopOnly.find(topVirtualSite), sitesTopOnly.find(xy));
         }
         if (row == size) {
             sites.union(sites.find(bottomVirtualSite), sites.find(xy));
@@ -43,6 +45,7 @@ public class Percolation {
             int xy1 = xyTo1D(col1, row1);
             if (isInside(row1, col1) && opened[xy1]) {
                 sites.union(sites.find(xy), sites.find(xy1));
+                sitesTopOnly.union(sitesTopOnly.find(xy), sitesTopOnly.find(xy1));
             }
         }
     }
@@ -55,7 +58,7 @@ public class Percolation {
     // is site (row i, column j) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
-        return sites.connected(topVirtualSite, xyTo1D(col, row));
+        return sitesTopOnly.connected(topVirtualSite, xyTo1D(col, row));
     }
 
     // is site (row i, column j) open?
