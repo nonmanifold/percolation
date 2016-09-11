b511b48fb22dbaaeef46d335a7bd6e14aaa3ff33
nick serebrennikov <non.manifold@gmail.com>
2016-09-11 21:13:22 +0300

    add code to compute mean and other stats metrics

--- src/pro/manifold/percolation/PercolationStats.java
+++ src/pro/manifold/percolation/PercolationStats.java
@@ -1,9 +1,13 @@
 package pro.manifold.percolation;
 
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
+
+    private final double[] xis;
+    private final int n;
+    private final int n2;
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
@@ -22,26 +26,68 @@ public class PercolationStats {
         if (trials <= 0) {
             throw new IllegalArgumentException("trials must be greater than 0");
         }
+        this.n = n;
+        this.n2 = n * n;
+        xis = new double[trials];
+        for (int t = 0; t < trials; t++) {
+            xis[t] = fillTillPercolates();
+        }
+    }
+
+    private double fillTillPercolates() {
+        Percolation perc = new Percolation(n);
+        int xi = 0;
+        // initialize an array of two-element arrays with all the fillable cells indecies
+        int[][] canBeBloked = new int[n * n][];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                canBeBloked[row * n + col] = new int[]{row + 1, col + 1};
+            }
+        }
+        int canBeBlokedCount = canBeBloked.length;
+        while (!perc.percolates()) {
+            int siteToFillIdx = StdRandom.uniform(canBeBlokedCount);
+            int row = canBeBloked[siteToFillIdx][0];
+            int col = canBeBloked[siteToFillIdx][1];
+
+            //swap used element outside of working set:
+            canBeBloked[siteToFillIdx] = canBeBloked[canBeBlokedCount - 1];
+            canBeBlokedCount--; // move pointer to the working set far end
+            perc.open(row, col);
+            xi++;// advance counter
+        }
+        return xi / (double) n2;
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0;
+        double xiSum = 0;
+        for (double xi : xis) {
+            xiSum += xi;
+        }
+        return xiSum / (double) xis.length;
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0;
+        double mean = mean();
+        double xiSumMinusMean = 0;
+        for (double xi : xis) {
+            xiSumMinusMean += (xi - mean) * (xi - mean);
+        }
+        return Math.sqrt(xiSumMinusMean / ((double) xis.length - 1));
     }
 
     // low  endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 0;
+        double mean = mean();
+        return mean - 1.96 * stddev() / Math.sqrt((double) xis.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0;
+        double mean = mean();
+        return mean + 1.96 * stddev() / Math.sqrt((double) xis.length);
     }
 
 }
--- tests/pro/manifold/percolation/PercolationStatsTest.java
+++ tests/pro/manifold/percolation/PercolationStatsTest.java
@@ -25,19 +25,27 @@ public class PercolationStatsTest {
     @Test
     public void test200x100() throws Exception {
         PercolationStats stats = new PercolationStats(200, 100);
-        assertTrue("mean, ~0.59", closeTo(0.59, stats.mean(), 0.02));
-        assertTrue("stddev, ~0.008", closeTo(0.01, stats.stddev(), 0.01));
-        assertTrue("95% confidence interval low >0.55", 0.55 >= stats.confidenceLo());
-        assertTrue("95% confidence interval high <0.65", 0.60 <= stats.confidenceLo());
+        double mean = stats.mean();
+        assertTrue("mean, ~0.59", closeTo(0.59, mean, 0.05));
+        double stddev = stats.stddev();
+        assertTrue("stddev, ~0.008", closeTo(0.01, stddev, 0.01));
+        double confidenceLo = stats.confidenceLo();
+        assertTrue("95% confidence interval low >0.55", confidenceLo >= 0.55);
+        double confidenceHi = stats.confidenceHi();
+        assertTrue("95% confidence interval high <0.65", confidenceHi <= 0.60);
     }
 
     @Test
     public void test2x100000() throws Exception {
         PercolationStats stats = new PercolationStats(2, 100000);
-        assertTrue("mean, ~0.66", closeTo(0.66, stats.mean(), 0.02));
-        assertTrue("stddev, ~0.11", closeTo(0.11, stats.stddev(), 0.02));
-        assertTrue("95% confidence interval low >0.60", 0.60 >= stats.confidenceLo());
-        assertTrue("95% confidence interval high <0.70", 0.70 <= stats.confidenceLo());
+        double mean = stats.mean();
+        assertTrue("mean, ~0.66", closeTo(0.66, mean, 0.05));
+        double stddev = stats.stddev();
+        assertTrue("stddev, ~0.11", closeTo(0.11, stddev, 0.02));
+        double confidenceLo = stats.confidenceLo();
+        assertTrue("95% confidence interval low >0.60", confidenceLo >= 0.60);
+        double confidenceHi = stats.confidenceHi();
+        assertTrue("95% confidence interval high <0.70", confidenceHi <= 0.70);
     }
 
     private boolean closeTo(double expected, double actual, double alpha) {
